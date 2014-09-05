c46a6658bcf700e813ea403b6fae34088b494df7
markkinchin <markkinchin@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-05 14:08:44 +0000

    2.0.7
    git-svn-id: https://plugins.svn.wordpress.org/beds24-online-booking/tags/2.0.7@981855 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ plugin-languages.php
@@ -0,0 +1,20 @@
+<h2>Languages</h2>
+<h3 class="b24_heading">The plugin supports multiple lanugages</h3>
+<div class="b24_section section-text">
+<p>Languages can be set directly in the shortcode. This allows using multiple languages on one web site.</p>
+<strong>Embedded booking page</strong>
+<p>Exapmles:</p>
+<p>Default language shortcode for embedded booking page: [beds24-embed] </p>
+<p>French language shortcode for embedded booking page: [beds24-embed lang="fr"] </p>
+<p>Click <a target="_blank" href="http://wiki.beds24.com/index.php?title=Page/widgetwebdesign">here for a list of all language parameters</a>.
+</p>
+<strong>Booking Box</strong>
+<p>Exapmles:</p>
+<p>Default language shortcode for booking box: [beds24-box] </p>
+<p>French language shortcode for booking box: [beds24-box lang="fr"] </p>       
+<p>The booking box is available in English (en), German (de), French (fr), Italian (it) and Portugese (pt). If you need another language please contact us via the ticket support system in the Beds24 control panel.</p>
+<strong>Booking Button or Link</strong> 
+<p>Default language shortcode for booking box: [beds24-button] </p>
+<p>French language shortcode for booking box: [beds24-button text="Rechercher"]  </p>
+<p>Just add the text you want to display to the shortcode</p>
+</div>
