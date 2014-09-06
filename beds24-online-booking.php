19c476fd9f3e1fa9535bfa331f6c565d2a1c460f
markkinchin <markkinchin@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-06 17:10:29 +0000

    2.0.7
    git-svn-id: https://plugins.svn.wordpress.org/beds24-online-booking/trunk@983126 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- beds24-online-booking.php
+++ beds24-online-booking.php
@@ -3,7 +3,7 @@
 Plugin Name: Beds24 Online Booking
 Plugin URI: http://www.beds24.com
 Description: Beds24.com is a full featured online booking engine. The system is very flexible with many options for customization. The Beds24.com online booking system and channel manager is suitable for any type of accommodation such as hotels, motels, B&B's, hostels, vacation rentals, holiday homes and campgrounds as well as selling extras like tickets or tours. The plugin is free to use but you do need an account with Beds24.com. A free trial account is available <a href="http://www.beds24.com/join.html" target="_blank">here</a>
-Version: 2.0.6
+Version: 2.0.7
 Author: Mark Kinchin
 Author URI: http://www.beds24.com
 License: GPL2 or later
