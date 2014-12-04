d9be5e2483220cf10577c2f5b848154976f779e4
markkinchin <markkinchin@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-04 16:21:06 +0000

    version 2.0.9
    git-svn-id: https://plugins.svn.wordpress.org/beds24-online-booking/tags/2.0.8@1038256 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ beds24-translations.php
@@ -0,0 +1,140 @@
+<?php
+switch ($lang)
+  {
+  case 'de': //german
+    $translate['Check In'] = 'Check-in';
+    $translate['Day'] = 'Tag';
+    $translate['Guests'] = 'Gaeste';
+    $translate['Month'] = 'Monat';
+    $translate['month1'] = 'Jan';
+    $translate['month2'] = 'Feb';
+    $translate['month3'] = 'Mar';
+    $translate['month4'] = 'Apr';
+    $translate['month5'] = 'Mai';
+    $translate['month6'] = 'Jun';
+    $translate['month7'] = 'Jul';
+    $translate['month8'] = 'Aug';
+    $translate['month9'] = 'Sep';
+    $translate['month10'] = 'Okt';
+    $translate['month11'] = 'Nov';
+    $translate['month12'] = 'Dec';
+    $translate['more search options'] = 'Weitere Suchoptionen';
+    $translate['Nights'] = 'Nächte';
+    $translate['Search'] = 'Suchen';
+    $translate['Search Accommodation'] = 'Suche';
+        
+    break;
+   case 'es': //spanish
+    $translate['Check In'] = 'Check-in';
+    $translate['Day'] = 'Dia';
+    $translate['Guests'] = 'Huéspedes';
+    $translate['Month'] = 'Mes';
+    $translate['month1'] = 'ene';
+    $translate['month2'] = 'feb';
+    $translate['month3'] = 'mar';
+    $translate['month4'] = 'abr';
+    $translate['month5'] = 'may';
+    $translate['month6'] = 'jun';
+    $translate['month7'] = 'jul';
+    $translate['month8'] = 'ago';
+    $translate['month9'] = 'sep';
+    $translate['month10'] = 'oct';
+    $translate['month11'] = 'nov';
+    $translate['month12'] = 'dic';
+    $translate['more search options'] = 'Opciones de búsqueda adicionales';
+    $translate['Nights'] = 'Noches';
+    $translate['Search'] = 'Buscar';
+    $translate['Search Accommodation'] = 'Buscar';
+    break;
+        
+         case 'fr': //french
+    $translate['Check In'] = 'Check-in';
+    $translate['Day'] = 'Jour';
+    $translate['Guests'] = 'Clients';
+    $translate['Month'] = 'Mois';
+    $translate['month1'] = 'jan';
+    $translate['month2'] = 'fév';
+    $translate['month3'] = 'mar';
+    $translate['month4'] = 'avr';
+    $translate['month5'] = 'mai';
+    $translate['month6'] = 'juin';
+    $translate['month7'] = 'juil';
+    $translate['month8'] = 'août';
+    $translate['month9'] = 'sep';
+    $translate['month10'] = 'oct';
+    $translate['month11'] = 'nov';
+    $translate['month12'] = 'déc';
+    $translate['more search options'] = 'Autres options de recherche';
+    $translate['Nights'] = 'Nuits';
+    $translate['Search'] = 'Rechercher';
+    $translate['Search Accommodation'] = 'Rechercher';
+    break;
+        case 'it': //italian
+    $translate['Check In'] = 'Check-in';
+    $translate['Day'] = 'Giorno';
+    $translate['Guests'] = 'Ospiti';
+    $translate['Month'] = 'Mese';
+    $translate['month1'] = 'gen';
+    $translate['month2'] = 'feb';
+    $translate['month3'] = 'mar';
+    $translate['month4'] = 'apr';
+    $translate['month5'] = 'mag';
+    $translate['month6'] = 'gui';
+    $translate['month7'] = 'lug';
+    $translate['month8'] = 'ago';
+    $translate['month9'] = 'set';
+    $translate['month10'] = 'ott';
+    $translate['month11'] = 'nov';
+    $translate['month12'] = 'dic';
+    $translate['more search options'] = 'Ulteriori opzioni di ricerca';
+    $translate['Nights'] = 'Notti';
+    $translate['Search'] = 'Cerca';
+    $translate['Search Accommodation'] = 'Cerca';
+    break;
+        
+        case 'pt': //portugese
+    $translate['Check In'] = 'Entrada';
+    $translate['Day'] = 'Dia';
+    $translate['Guests'] = 'Hóspedes';
+    $translate['Month'] = 'Mes';
+    $translate['month1'] = 'jan';
+    $translate['month2'] = 'fev';
+    $translate['month3'] = 'mar';
+    $translate['month4'] = 'abr';
+    $translate['month5'] = 'mai';
+    $translate['month6'] = 'jun';
+    $translate['month7'] = 'jul';
+    $translate['month8'] = 'ago';
+    $translate['month9'] = 'set';
+    $translate['month10'] = 'out';
+    $translate['month11'] = 'nov';
+    $translate['month12'] = 'dez';
+    $translate['more search options'] = 'Opções adicionais de pesquisa';
+    $translate['Nights'] = 'Noites';
+    $translate['Search'] = 'Pesquisar';
+    $translate['Search Accommodation'] = 'Pesquisar';
+    break;
+        
+  default: //english
+    $translate['Check In'] = 'Check In';
+    $translate['Day'] = 'Day';
+    $translate['Guests'] = 'Guests';
+    $translate['Month'] = 'Month';
+    $translate['month1'] = 'Jan';
+    $translate['month2'] = 'Feb';
+    $translate['month3'] = 'Mar';
+    $translate['month4'] = 'Apr';
+    $translate['month5'] = 'May';
+    $translate['month6'] = 'Jun';
+    $translate['month7'] = 'Jul';
+    $translate['month8'] = 'Aug';
+    $translate['month9'] = 'Sep';
+    $translate['month10'] = 'Oct';
+    $translate['month11'] = 'Nov';
+    $translate['month12'] = 'Dec';
+    $translate['more search options'] = 'more search options';
+    $translate['Nights'] = 'Nights';
+    $translate['Search'] = 'Search';
+    $translate['Search Accommodation'] = 'Search Accommodation';
+    break;
+  }
---
+++ plugin-bookingpage.php
@@ -0,0 +1,185 @@
+<h2>Embedded Booking Page</h2>
+<div class="b24_section section-text">
+<h3 class="b24_heading">Customise and Install your Embedded Booking Page.</h3>
+<p>These settings customise your embedded booking page. To change fonts and colours log into your Beds24 account and go to SETTINGS -> BOOKING PAGE -> PAGE DESIGN</p>
+    
+<p>Paste the shortcode into the page where you want to embed your booking page. </p>    
+<table width="900" cellspacing="0" cellpadding="2" border="1">
+<tbody>
+<tr>
+<th width="50%">Description</th>
+<th>Shortcode</th>
+</tr>
+<tr>
+<td width="50%">embedded booking page</td>
+<td>[beds24-embed]</td>
+</tr>
+</tbody>
+</table>          
+</div>
+<div>
+<table>
+<tr valign="top">
+<td style="width: 160px; padding: 5px 5px 7px 5px;">Beds24 Owner Id:</td>
+<td style="padding: 5px 5px 7px 5px;">
+<input name="beds24_ownerid" type="text" id="beds24_ownerid" size=6 maxlength=100 value="<?php echo get_option('beds24_ownerid'); ?>" /></td>
+<td style="padding: 5px 5px 7px 5px;">
+<span style="font-style: italic; color: gray;"> Enter your Beds24.com owner Id number to create an embedded booking page show all properties in your account. You will find this in the Beds24 control panel at SETTINGS -> ACCOUNT "Account Number" </span>
+</td>
+</tr>
+
+<tr valign="top">
+<td style="width: 160px; padding: 5px 5px 7px 5px;">Beds24 Property Id:</td>
+<td style="padding: 5px 5px 7px 5px;">
+<input name="beds24_propid" type="text" id="beds24_propid" size=6 maxlength=100 value="<?php echo get_option('beds24_propid'); ?>" /></td>
+<td style="padding: 5px 5px 7px 5px;">
+<span style="font-style: italic; color: gray;"> Enter your Beds24.com Property Id number to create an embedded booking page for this property only. You will find this in the Beds24 control panel at SETTINGS -> PROPERTIES.</span>
+</td>
+</tr>
+
+<tr valign="top">
+<td style="padding: 5px 5px 7px 5px;">Width:</td>
+<td style="padding: 5px 5px 7px 5px;">
+<select name="beds24_width" id="beds24_width">
+<?php for($i = 200; $i < 1200; $i += 10) { ?>
+<option value ="<?php echo $i; ?>" <?php if(get_option('beds24_width') == $i) echo "selected"; ?>><?php echo $i; ?>px</option>
+<?php } ?>
+</select>
+</td>
+<td style="padding: 5px 5px 7px 5px;"> <span style="font-style: italic; color: gray;">Enter the width of the Embedded Booking Page in pixels.</span>
+</td>
+</tr>
+
+<tr valign="top">
+<td style="padding: 5px 5px 7px 5px;">Height:</td>
+<td style="padding: 5px 5px 7px 5px;">
+<select name="beds24_height" id="beds24_height">
+<?php for($i = 400; $i < 3000; $i += 10) { ?>
+<option value ="<?php echo $i; ?>" <?php if(get_option('beds24_height') == $i) echo "selected"; ?>><?php echo $i; ?>px</option>
+<?php } ?>
+</select></td>
+<td style="padding: 5px 5px 7px 5px;"> <span style="font-style: italic; color: gray;">Enter the height of the Embedded Booking Page in pixels. If you do not allow enough height the Embedded Booking Page will create a vertical scroll bar.</span>
+</td>
+</tr>
+
+<tr valign="top">
+<td style="padding: 5px 5px 7px 5px;">Number of Dates Displayed:</td>
+<td style="padding: 5px 5px 7px 5px;">
+<select name="beds24_numdisplayed" id="beds24_numdisplayed">
+<option value ="-1" <?php if(get_option('beds24_numdisplayed') == -1) echo "selected"; ?>>Default</option>
+<?php for($i = 0; $i <= 14; $i += 1) { ?>
+<option value ="<?php echo $i; ?>" <?php if(get_option('beds24_numdisplayed') == $i) echo "selected"; ?>><?php echo $i; ?></option>
+<?php } ?>
+</select></td>
+<td style="padding: 5px 5px 7px 5px;"> <span style="font-style: italic; color: gray;">Enter the number of date columns to display. Choose the number of columns to suit the width you have available.</span>
+</td>
+</tr>
+
+<tr valign="top">
+<td style="padding: 5px 5px 7px 5px;">Show Calendar:</td>
+<td style="padding: 5px 5px 7px 5px;">
+<select name="beds24_hidecalendar" id="beds24_hidecalendar">
+<option value ="-1" <?php if(get_option('beds24_hidecalendar') == -1) echo "selected"; ?>>Default</option>
+<option value ="1" <?php if(get_option('beds24_hidecalendar') == 1) echo "selected"; ?>>No</option>
+<option value ="0" <?php if(get_option('beds24_hidecalendar') == 0) echo "selected"; ?>>Yes</option>
+</select></td>
+<td style="padding: 5px 5px 7px 5px;"> <span style="font-style: italic; color: gray;">Show a monthly calendar on the booking page.</span>
+</td>
+</tr>
+
+<tr valign="top">
+<td style="padding: 5px 5px 7px 5px;">Show Header:</td>
+<td style="padding: 5px 5px 7px 5px;">
+<select name="beds24_hideheader" id="beds24_hideheader">
+<option value ="-1" <?php if(get_option('beds24_hideheader') == -1) echo "selected"; ?>>Default</option>
+<option value ="1" <?php if(get_option('beds24_hideheader') == 1) echo "selected"; ?>>No</option>
+<option value ="0" <?php if(get_option('beds24_hideheader') == 0) echo "selected"; ?>>Yes</option>
+</select></td>
+<td style="padding: 5px 5px 7px 5px;"> <span style="font-style: italic; color: gray;">Show the booking page header.</span>
+</td>
+</tr>
+
+<tr valign="top">
+<td style="padding: 5px 5px 7px 5px;">Show Footer:</td>
+<td style="padding: 5px 5px 7px 5px;">
+<select name="beds24_hidefooter" id="beds24_hidefooter">
+<option value ="-1" <?php if(get_option('beds24_hidefooter') == -1) echo "selected"; ?>>Default</option>
+<option value ="1" <?php if(get_option('beds24_hidefooter') == 1) echo "selected"; ?>>No</option>
+<option value ="0" <?php if(get_option('beds24_hidefooter') == 0) echo "selected"; ?>>Yes</option>
+</select></td>
+<td style="padding: 5px 5px 7px 5px;"> <span style="font-style: italic; color: gray;">Show the booking page footer.</span>
+</td>
+</tr>
+
+<tr valign="top">
+<td style="padding: 5px 5px 7px 5px;">Days in Advance:</td>
+<td style="padding: 5px 5px 7px 5px;">
+<select name="beds24_advancedays" id="beds24_advancedays">
+<option value ="-1" <?php if(get_option('beds24_advancedays') == -1) echo "selected"; ?>>First Available</option>
+<?php for($i = 0; $i <= 180; $i += 1) { ?>
+<option value ="<?php echo $i; ?>" <?php if(get_option('beds24_advancedays') == $i) echo "selected"; ?>><?php echo $i; ?></option>
+<?php } ?>
+</select></td>
+<td style="padding: 5px 5px 7px 5px;"> <span style="font-style: italic; color: gray;">This is the number of days ahead of the current date for the default checkin date. For example if you set it to one, the default date will always be tomorrow. This only applies the first time the page opens, once opened or re-opened it will remember the previously selected date.</span>
+</td>
+</tr>
+
+<tr valign="top">
+<td style="padding: 5px 5px 7px 5px;">Number of Nights:</td>
+<td style="padding: 5px 5px 7px 5px;">
+<select name="beds24_numnight" id="beds24_numnight">
+<?php for($i = 1; $i <= 7; $i += 1) { ?>
+<option value ="<?php echo $i; ?>" <?php if(get_option('beds24_numnight') == $i) echo "selected"; ?>><?php echo $i; ?></option>
+<?php } ?>
+</select></td>
+<td style="padding: 5px 5px 7px 5px;"> <span style="font-style: italic; color: gray;">This is the default setting for the booking duration.</span>
+</td>
+</tr>
+
+<tr valign="top">
+<td style="padding: 5px 5px 7px 5px;">Number of Guests:</td>
+<td style="padding: 5px 5px 7px 5px;">
+<select name="beds24_numadult" id="beds24_numadult">
+<?php for($i = 1; $i <= 8; $i += 1) { ?>
+<option value ="<?php echo $i; ?>" <?php if(get_option('beds24_numadult') == $i) echo "selected"; ?>><?php echo $i; ?></option>
+<?php } ?>
+</select></td>
+<td style="padding: 5px 5px 7px 5px;"> <span style="font-style: italic; color: gray;">This is the default setting for the number of guests.</span>
+</td>
+</tr>
+
+<tr valign="top">
+<td style="padding: 5px 5px 7px 5px;">Referer:</td>
+<td style="padding: 5px 5px 7px 5px;">
+<input type="text" name="beds24_referer" id="beds24_referer" value="<?php echo str_replace('"', "", get_option('beds24_referer')); ?>">
+</td>
+<td style="padding: 5px 5px 7px 5px;"> <span style="font-style: italic; color: gray;">Store this text as the referer for bookings originating here.</span>
+</td>
+</tr>
+
+<tr valign="top">
+<td style="padding: 5px 5px 7px 5px;">Booking Page Domain:</td>
+<td style="padding: 5px 5px 7px 5px;">
+<input type="text" name="beds24_domain" id="beds24_domain" value="<?php echo str_replace('"', "", get_option('beds24_domain')); ?>">
+</td>
+<td style="padding: 5px 5px 7px 5px;"> <span style="font-style: italic; color: gray;">The domain hosting your booking page, normally https://www.beds24.com</span>
+</td>
+</tr>
+
+<tr valign="top">
+<td style="padding: 5px 5px 7px 5px;">Custom URL Parameters:</td>
+<td style="padding: 5px 5px 7px 5px;">
+<input type="text" name="beds24_custom" id="beds24_custom" value="<?php echo str_replace('"', "", get_option('beds24_custom')); ?>">
+</td>
+<td style="padding: 5px 5px 7px 5px;"> <span style="font-style: italic; color: gray;">Add custom parameters to the booking page URL. See <a href="http://wiki.beds24.com/index.php?title=Page/widgetwebdesign" target="_blank">here</a> for more information.</span>
+</td>
+</tr>
+
+<tr valign="top">
+<td style="padding: 5px 5px 7px 5px;"></td>
+<td colspan="2" style="padding: 5px 5px 7px 5px;">
+<input type="submit" value="<?php _e('Save Changes') ?>" />
+</td>
+</tr>
+</table>                     
+</div>   
